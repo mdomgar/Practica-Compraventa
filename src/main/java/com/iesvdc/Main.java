9c67e9479985016a9de5455bd4aee360407cbd87
mdomgar <mdomgar413s@g.educaand.es>
2024-10-16 17:40:02 +0200

    Subida de pratica

--- src/main/java/com/iesvdc/Main.java
+++ src/main/java/com/iesvdc/Main.java
@@ -3,7 +3,6 @@ package com.iesvdc;
 import com.iesvdc.controller.CocheController;
 import com.iesvdc.model.Catalogo;
 import com.iesvdc.model.CocheManager;
-import com.iesvdc.view.MainFrame;
 import com.iesvdc.view.MainFrame23424;
 
 import javax.swing.*;
